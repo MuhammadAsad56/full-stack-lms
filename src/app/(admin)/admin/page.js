3151481b2cbb167f276dddf4cd87c2fd04c02ba2
Muhammad Asad <155156442+MuhammadAsad56@users.noreply.github.com>
2024-10-24 09:04:47 -0700

    message

---
+++ src/app/(admin)/admin/page.js
@@ -0,0 +1,7 @@
+export default function Admin (){
+    return(
+        <div className="min-h-screen">
+            <h1 className="text-center text-2xl my-10 font-bold">Admin</h1>
+        </div>
+    )
+}
